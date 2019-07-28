1e2f7af5c16e2c42ec324e2076fc147d02534de2
rowa-codes <raowaam@gmail.com>
2019-07-28 14:26:13 +0300

    first commit
    -Map integrated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDonQhgJpBJtRi7JaM1T0nycwkJt7_MD4g&v=2&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+     var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 36.316666, lng: 74.649986},
+      zoom: 15
+    })
+  }
+
+  render() {
+    return (
+      <main>
+         <div id="map"></div>
+      </main>
+      )
+  }
 }
+
+
+function loadScript(url) {
+  var index  = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
 
 export default App;
